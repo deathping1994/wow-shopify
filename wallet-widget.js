dfe10522cb3102f6d1eb9e0fc6c6ac3961bc3eae
Divyam55 <64700915+Divyam55@users.noreply.github.com>
2023-04-25 20:31:53 +0530

    Create wallet-widget.js

---
+++ wallet-widget.js
@@ -0,0 +1,476 @@
+$('body').append(`
+
+<div class="wonder_points XXsnipcss_extracted_selector_selectionXX tether-target-attached-top tether-element-attached-top tether-element-attached-center tether-target-attached-center">
+   <div id="card-container" class="wonder_points__overlay show_overlay" style="display: none; background: url(&quot;https://wonderchef-media.farziengineer.co/hosted/cross-hatching-e3e3a5431f70.svg&quot;) repeat rgb(255, 255, 255);">
+      <div class="header">
+         <img id="header-close" class="close" src="https://wonderchef-media.farziengineer.co/hosted/close-604e01f71fd5.svg" alt="close header" width="30px">
+         <div class="">
+            <h1 class="heading">Login to access</h1>
+            <p>Wonderpoints Wallet</p>
+         </div>
+      </div>
+      <div class="content">
+         <div class="cardWrapper">
+            <h1 class="">Become a part of the Wonderchef Family</h1>
+            <p class="">Earn Wonderpoints every time you purchase. Unlock exciting discounts.</p>
+            <a class="joinBtn" target="_blank" rel="noopener noreferrer" href="/account/register"> Join now </a>
+            <p class="">Already have an account? <a href="/account/login" class="">Sign in</a></p>
+         </div>
+         <div class="cardWrapper">
+            <h1 class="">Wonderpoints</h1>
+            <p class="">Earn more Wonderpoints for different actions, and turn those Wonderpoints into awesome rewards!</p>
+            <div class="dropDown">
+               <div class="dropDown__content">
+                  <p class="">way to earn </p>
+               </div>
+               <div class="">
+                  <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg" class="">
+                     <circle cx="17" cy="17" r="17" fill="white"></circle>
+                     <path d="M14 11C14.3 11.3 18.125 15.125 20 17L14 23" stroke="#2C2C2C" stroke-width="2" stroke-linecap="round"></path>
+                  </svg>
+               </div>
+            </div>
+            <div class="dropDown">
+               <div class="dropDown__content">
+                  <p class="">way to redeem </p>
+               </div>
+               <div class="">
+                  <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg" class="">
+                     <circle cx="17" cy="17" r="17" fill="white"></circle>
+                     <path d="M14 11C14.3 11.3 18.125 15.125 20 17L14 23" stroke="#2C2C2C" stroke-width="2" stroke-linecap="round"></path>
+                  </svg>
+               </div>
+            </div>
+         </div>
+         <div class="cardWrapper">
+            <h1 class="">Referrals</h1>
+            <p class="">Give your friends an assured reward and receive your own when they make their first purchase. Can be used on any purchase above Rs. 2500</p>
+            <div class="dropDown removeBanner">
+               <div class="dropDown__content">
+                  <div class="">
+                     <p class="">They get </p>
+                     <p class="">20% off coupon</p>
+                  </div>
+               </div>
+            </div>
+            <div class="dropDown removeBanner">
+               <div class="dropDown__content">
+                  <div class="">
+                     <p class="">You get </p>
+                     <p class="">1000 Wonderpoints</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="powered-by-content">
+         <span class="powered-by-content__text">powered By</span>
+         <div class="scss_ImageWrapper___u3k0" style="display: flex; align-items: center; justify-content: center;">
+            <img src="https://wowshopify.farziengineer.co/felogo.png" style="width:5rem">
+          </div>
+      </div>
+   </div>
+   <div id="widget-container" class="wonder_points__gifting">
+      <div class="scss_ImageWrapper___u3k0" style="display: flex; align-items: center; justify-content: center;">
+        <img src="https://wowshopify.farziengineer.co/wallet4.png">
+      </div>
+   </div>
+</div>
+<style>
+  
+   #widget-img{
+       position: absolute;
+       inset: 0px;
+       box-sizing: border-box;
+       padding: 0px;
+       border: none;
+       margin: auto;
+       display: block;
+       width: 0px;
+       height: 0px;
+       min-width: 50%;
+       max-width: 50%;
+       min-height: 50%;
+       max-height: 50%;
+   }
+   #widget-button {
+       background-color: #4CAF50;
+       color: white;
+       padding: 10px 20px;
+       border: none;
+       border-radius: 3px;
+       cursor: pointer;
+   }
+   .wonder_points {
+       -ms-flex-align: end;
+       align-items: flex-end;
+       -ms-flex-direction: column;
+       flex-direction: column;
+       position: fixed;
+       bottom: 2%;
+       right: 3%;
+       z-index: 3;
+       display: -ms-flexbox;
+       display: flex
+   }
+   .wonder_points__overlay {
+       width: 315px;
+       height: 50rem;
+       position: relative;
+       bottom: 85%;
+       right: 18%;
+       -webkit-transition: all .5s;
+       transition: all .5s;
+       -webkit-box-shadow: 0 .0625em .0625em rgba(0,0,0,.25),0 .125em .5em rgba(0,0,0,.25),inset 0 0 0 1px hsla(0,0%,100%,.1);
+       box-shadow: 0 .0625em .0625em rgba(0,0,0,.25),0 .125em .5em rgba(0,0,0,.25),inset 0 0 0 1px hsla(0,0%,100%,.1);
+       border-radius: 5px;
+       background-color: #fff;
+       overflow-y: scroll;
+       -ms-overflow-style: none;
+       scrollbar-width: none
+   }
+   .wonder_points__overlay::-webkit-scrollbar {
+       display: none
+   }
+   .wonder_points__overlay .powered-by-content {
+       -ms-flex-pack: center;
+       justify-content: center;
+       display: -ms-flexbox;
+       display: flex;
+       padding: .5rem;
+       font-size: 1rem;
+       text-transform: capitalize;
+       text-align: center;
+       z-index: 2;
+       background-color: #fff;
+       -webkit-box-shadow: 0 2px 4px 0 rgba(14,30,37,.12),0 2px 16px 0 rgba(14,30,37,.32);
+       box-shadow: 0 0 4px 0 rgba(14,30,37,.12),0 2px 16px rgba(14,30,37,.32)
+   }
+   .wonder_points__overlay .powered-by-content__text {
+       margin-right: .5rem
+   }
+   .wonder_points__overlay .close {
+       position: absolute;
+       right: .8rem;
+       top: 10px;
+       cursor: pointer
+   }
+   .wonder_points__overlay .fixedHeader {
+       background: linear-gradient(135.19deg,#989898,#000000);
+       padding: 1rem;
+       position: sticky;
+       top: 0
+   }
+   .wonder_points__overlay .heading {
+       line-height: 1.9;
+       font-size: 1.6rem;
+       color: #fff;
+        margin: 0.5rem;
+   }
+   .wonder_points__overlay .wallet-amount {
+       font-size: 1rem
+   }
+   .wonder_points__overlay .header {
+       background: linear-gradient(135.19deg,#989898,#000000);
+       width: 100%;
+       z-index: 2;
+       padding: 1rem;
+       min-height: 4rem;
+       height: 10rem;
+       -ms-flex-pack: start;
+       justify-content: flex-start;
+        align-items: baseline;
+   }
+   .wonder_points__overlay .header p {
+       color: #fff;
+       font-size: 1.3rem;
+       font-weight: 500;
+        margin: 0rem 0.5rem;
+   }
+   .wonder_points__overlay .content {
+       grid-gap: 3rem;
+       gap: 3rem;
+       padding-bottom: 2rem;
+       min-height: 19.5rem
+   }
+   .wonder_points__overlay .content,.wonder_points__overlay .content .cardWrapper {
+       display: -ms-flexbox;
+       display: flex;
+       -ms-flex-direction: column;
+       flex-direction: column
+   }
+   .wonder_points__overlay .content .cardWrapper {
+       z-index: 2;
+       background-color: #fff;
+       -webkit-box-shadow: 0 2px 4px 0 rgba(14,30,37,.12),0 2px 16px 0 rgba(14,30,37,.32);
+       box-shadow: 0 2px 4px 0 rgba(14,30,37,.12),0 2px 16px 0 rgba(14,30,37,.32);
+       width: 90%;
+       margin: -2rem auto 0;
+       border-radius: 4px;
+       min-height: -webkit-fit-content;
+       min-height: -moz-fit-content;
+       min-height: fit-content;
+       padding: 1rem;
+       -ms-flex-align: center;
+       align-items: center;
+       grid-gap: .8rem;
+       gap: .8rem
+   }
+   .wonder_points__overlay .content .cardWrapper h1 {
+       font-size: 1.3rem;
+        color: #000;
+        font-weight: 600;
+        line-height: 1.2;
+   }
+   .wonder_points__overlay .content .cardWrapper p {
+       font-size: 1rem;
+       color: #637381;
+       line-height: 1.3;
+       text-align: left;
+       padding: 0 1rem;
+       margin: 0rem;
+   }
+   .wonder_points__overlay .content .cardWrapper a {
+       color: #000000;
+       width: 100%
+   }
+   .wonder_points__overlay .content .cardWrapper .joinBtn {
+       width: -webkit-fit-content;
+       width: -moz-fit-content;
+       width: fit-content;
+       padding: .5rem 1rem;
+       background-color: #000000;
+       color: #fff;
+       border-radius: 8px
+   }
+   .wonder_points__overlay .content .cardWrapper .dropDown {
+       width: 100%;
+       -ms-flex-pack: justify;
+       justify-content: space-between;
+       padding-bottom: 5px;
+       position: relative;
+       cursor: pointer
+   }
+   .wonder_points__overlay .content .cardWrapper .dropDown,.wonder_points__overlay .content .cardWrapper .dropDown__content {
+       display: -ms-flexbox;
+       display: flex;
+       -ms-flex-align: center;
+       align-items: center
+   }
+   .wonder_points__overlay .content .cardWrapper .dropDown:after {
+       padding: 0;
+       margin: 0;
+       display: block;
+       content: "";
+       width: 30%;
+       height: 1px;
+       background-color: #fff;
+       position: absolute;
+       left: 0;
+       bottom: -1px
+   }
+   .wonder_points__overlay .content .cardWrapper .removeBanner {
+       border: 0!important;
+       cursor: default
+   }
+   .wonder_points__overlay .content .card_blocked {
+       opacity: .5
+   }
+   .wonder_points__overlay .content .refer-sub-section__referal-link {
+       width: 100%
+   }
+   .wonder_points__overlay .content .refer-sub-section__referral-section {
+       display: -ms-flexbox;
+       display: flex;
+       -ms-flex-direction: column;
+       flex-direction: column;
+       -ms-flex-align: start;
+       align-items: flex-start;
+       width: 100%;
+       padding: 7px;
+       border-radius: 4px
+   }
+   .wonder_points__overlay .content .refer-sub-section__referral-section__heading {
+       font-size: 1rem!important
+   }
+   .wonder_points__overlay .content .refer-sub-section__referral-section__numberReferral {
+       padding: 0!important;
+       margin-top: .4rem;
+       font-size: .8rem!important
+   }
+   .wonder_points__overlay .content .refer-sub-section__referral-section:hover {
+       background-color: #fafafa
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__text-content {
+       font-size: .8rem!important
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__link-wrapper {
+       display: -ms-flexbox;
+       display: flex;
+       height: 2.5rem;
+       border-radius: 10px;
+       padding: .2rem .5rem;
+       border: 1px solid #e6e6e6;
+       color: #637381;
+       margin-top: 1rem
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__link-wrapper__refer-link {
+       height: 100%;
+       border: none;
+       outline: none;
+       -ms-flex: 1 1;
+       flex: 1 1
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__link-wrapper__refer-copy {
+       position: relative;
+       display: -ms-flexbox;
+       display: flex;
+       height: 100%;
+       padding: .5rem;
+       border-radius: 4px
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__link-wrapper__refer-copy:hover {
+       background-color: #fafafa
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__link-wrapper__refer-copy:hover .tooltiptext {
+       visibility: visible
+   }
+   .wonder_points__overlay .content .refer-sub-section__refer-link-section__link-wrapper__refer-copy .tooltiptext {
+       visibility: hidden;
+       width: 67px;
+       background-color: #000;
+       color: #fff;
+       text-align: center;
+       padding: 5px 0;
+       border-radius: 6px;
+       position: absolute;
+       z-index: 1;
+       top: -96%;
+       right: -30%;
+       opacity: .8
+   }
+   @media(max-width: 720px) {
+       .wonder_points__overlay {
+           right:12%
+       }
+   }
+   .wonder_points .show_overlay {
+       opacity: 1
+   }
+   .wonder_points .hide_overlay {
+       opacity: 0;
+       display: none
+   }
+   .wonder_points__gifting {
+       display: -ms-flexbox;
+       display: flex;
+       -ms-flex-align: center;
+       align-items: center;
+       width: 70px;
+       height: 70px;
+       border-radius: 50%;
+       background-color: #000000;
+       padding: 1rem;
+       cursor: pointer;
+       -webkit-box-shadow: 0 .0625em .0625em rgba(0,0,0,.25),0 .125em .5em rgba(0,0,0,.25),inset 0 0 0 1px hsla(0,0%,100%,.1);
+       box-shadow: 0 .0625em .0625em rgba(0,0,0,.25),0 .125em .5em rgba(0,0,0,.25),inset 0 0 0 1px hsla(0,0%,100%,.1)
+   }
+   .wonder_points__gifting img {
+       width: 85%
+   }
+   @media(max-width: 720px) {
+       .wonder_points__gifting {
+           width:50px;
+           height: 50px;
+           padding: .8rem;
+           z-index: 3
+       }
+   }
+   .wonder_points .joinBtn {
+       padding: .5rem 1rem;
+       background-color: #000000;
+       color: #fff;
+       border-radius: 8px;
+       cursor: pointer
+   }
+   .wonder_points .cardWrapper.coverFullScreen {
+       position: absolute;
+       top: 32px;
+       bottom: 0;
+       right: 0;
+       width: 100%!important;
+       -ms-flex-align: start!important;
+       align-items: flex-start!important;
+       height: 150%!important;
+       padding-bottom: 1rem!important
+   }
+   .wonder_points .cardWrapper.coverFullScreen h1 {
+       margin-bottom: 1rem;
+       font-size: 1.2rem!important;
+       display: -ms-flexbox;
+       display: flex;
+       -ms-flex-align: center;
+       align-items: center
+   }
+   .wonder_points .cardWrapper.coverFullScreen .banner-message {
+       background-color: #f4f6fb;
+       padding: 16px;
+       margin: .5rem auto 0
+   }
+   .wonder_points .cardWrapper.coverFullScreen.h-100 {
+       height: 100%!important
+   }
+   .wonder_points .join_bottom {
+       background-color: #fff;
+       position: fixed;
+       bottom: 75px;
+       height: 8rem;
+       padding: 2rem 0;
+       z-index: 2;
+       width: 315px;
+       display: -ms-flexbox;
+       display: flex;
+       -ms-flex-align: center;
+       align-items: center;
+       -ms-flex-direction: column;
+       flex-direction: column
+   }
+   .wonder_points .join_bottom .joinBtn {
+       width: 70%;
+       text-align: center;
+       margin-bottom: 1rem
+   }
+
+
+   .header {
+        display: flex;
+    }
+
+    .content {
+        font-size: 13px;
+    }
+    .content, .title {
+        margin-bottom: 2.5px;
+    }
+</style>
+
+`)
+var widgetButton = document.getElementById('widget-container');
+var cardContainer = document.getElementById('card-container');
+var headerClose = document.getElementById('header-close');
+
+widgetButton.addEventListener('click', function() {
+    if (cardContainer.style.display === 'none') {
+        cardContainer.style.display = 'block';
+    } else {
+        cardContainer.style.display = 'none';
+    }
+});
+
+headerClose.addEventListener('click', function() {
+    if (cardContainer.style.display === 'none') {
+        cardContainer.style.display = 'block';
+    } else {
+        cardContainer.style.display = 'none';
+    }
+});
